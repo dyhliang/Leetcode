79f75c55fbb82d7a3a0eb516fd32899d7085f479
dyhliang <102781030+dyhliang@users.noreply.github.com>
2022-06-14 11:41:50 -0400

    Time: 50 ms (87.62%), Space: 13.8 MB (75.11%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -2,8 +2,7 @@ class Solution:
     def romanToInt(self, s: str):
         vals = {"I": 1, "IV": 4, "V": 5, "IX": 9,
                 "X": 10, "XL": 40, "L": 50, "XC": 90,
-                "C": 100, "CD": 400, "D": 500, "CM": 900,
-                "M": 1000}
+                "C": 100, "CD": 400, "D": 500, "CM": 900, "M": 1000}
 
         digits = [char for char in s]
         total = 0
