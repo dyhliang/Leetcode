6c8f633a7d3dd367f7e9b374bc06949fbbaa27cd
dyhliang <102781030+dyhliang@users.noreply.github.com>
2022-06-14 00:31:49 -0400

    Time: 77 ms (5.12%), Space: 14 MB (15.00%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -1,18 +1,25 @@
 class Solution:
+    
+    def rev_append(self, li):
+        res = ""
+        for pos in range(len(li)):
+            if li[pos] == "0" and (pos == len(li) - 1):
+                res += ""
+            else:
+                res += li[pos]
+            
+        return res
+    
     def reverse(self, x: int) -> int:
-        if x in range(-9, 10):
-            return x
-        
+
         new_num = ""
-        if x >= 10:
+        if x == 0:
+            return 0
+        elif x >= 1:
             list_num = [digit for digit in str(x)]
             list_num.reverse()
             
-            for pos in range(len(list_num)):
-                if list_num[pos] == "0" and (pos == 0 or pos == len(list_num) - 1):
-                    new_num += ""
-                else:
-                    new_num += list_num[pos]
+            new_num = self.rev_append(list_num)
             
             if int(new_num) > (2 ** 31 - 1):
                 return 0
@@ -23,13 +30,9 @@ class Solution:
             list_num = [str_x[pos] for pos in range(1, len(str_x))]
             list_num.reverse()
             
-            for pos in range(0, len(list_num)):
-                if list_num[pos] == "0" and (pos == 0 or pos == len(list_num) - 1):
-                    new_num += ""
-                else:
-                    new_num += list_num[pos]
-              
-            if int(new_num) * -1 < -2147483648:
+            new_num = self.rev_append(list_num)
+            
+            if int(new_num) * -1 < (-2 ** 31):
                 return 0
             else:
                 return int(new_num) * -1
