cbead1be9988baef7a9443e0541b5f90ec519afb
dyhliang <102781030+dyhliang@users.noreply.github.com>
2022-06-14 01:54:28 -0400

    Time: 197 ms (16.96%), Space: 18.8 MB (45.45%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -4,18 +4,21 @@ class Solution:
         occ_table = {}
         mode_vals = []
 
+        # Fill up occurrences table
         if k in range(1, unique_vals + 1):
             for i in range(len(nums)):
                 occ_table[nums[i]] = 1 + occ_table.get(nums[i], 0)
         else:
             return []
-
+        
+        
+        # Make lists of all keys and values, then map to a new list for [key, occurrences], then sort
         keys_list = list(occ_table.keys())
         vals_list = list(occ_table.values())
-
         mapped_list = [[vals_list[pos], keys_list[pos]] for pos in range(len(keys_list))]
         mapped_list.sort()
 
+        # Iterate backwards from mapped_list by k times
         pos = -1
         while k > 0:
             mode_vals.append(mapped_list[pos][1])
