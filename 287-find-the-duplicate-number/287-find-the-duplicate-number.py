e46442f0d25dbb08dd64a7838d31f791911f091b
dyhliang <102781030+dyhliang@users.noreply.github.com>
2022-06-15 12:11:31 -0400

    Time: 669 ms (86.78%), Space: 32.2 MB (10.52%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -1,7 +1,6 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         occ = {}
-        
         for i in range(len(nums)):
             occ[nums[i]] = 1 + occ.get(nums[i], 0)
             if occ[nums[i]] == 2:
