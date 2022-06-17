c491870680143261c4bf4d4cf472d43ce9a9fc05
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-06-17 00:57:49 -0400

    Time: 511 ms (73.21%), Space: 25.8 MB (92.92%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -5,7 +5,6 @@ class Solution:
         # Use a hash table to store occurrences, jump out when an occurence happens more than once
         for i in range(len(nums)):
             occ_table[nums[i]] = 1 + occ_table.get(nums[i], 0)
-            if occ_table[nums[i]] == 2:
-                return True
+            if occ_table[nums[i]] == 2: return True
             
         return False
