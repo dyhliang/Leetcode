33419404af35ee99e7e9000eb52013840f028053
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-06-17 00:57:28 -0400

    Time: 726 ms (26.36%), Space: 25.9 MB (71.56%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,7 +1,7 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         
-        occ_table = dict()
+        occ_table = {}
         # Use a hash table to store occurrences, jump out when an occurence happens more than once
         for i in range(len(nums)):
             occ_table[nums[i]] = 1 + occ_table.get(nums[i], 0)
