024e5a01b289cebc0ea4f4ab4b0b14714ee3800c
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-06-22 13:48:45 -0400

    Time: 51 ms (37.50%), Space: 13.9 MB (23.72%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -3,15 +3,17 @@ class Solution:
         pairs = {")": "(",
                  "]": "[",
                  "}": "{"}
-
         helper = []
         for char in s:
             if char in pairs:
-                if helper and helper[-1] == pairs[char]:
+                # if the stack is not empty and the top of the stack == the value to the key, pop
+                # else, return
+                if helper and (helper[-1] == pairs[char]):
                     top = helper.pop()
                 else:
                     return False
             else:
+                # appends the left bracket onto stack
                 helper.append(char)
             
         return len(helper) == 0
