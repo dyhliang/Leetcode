7edbf77438b6f066e53777d72209266933bc887e
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-06-22 13:47:04 -0400

    Time: 50 ms (40.13%), Space: 13.9 MB (69.34%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        pairs = {")": "(",
+                 "]": "[",
+                 "}": "{"}
+        top = None
+        helper = []
+        for char in s:
+            if char in pairs:
+                if helper and helper[-1] == pairs[char]:
+                    top = helper.pop()
+                else:
+                    return False
+            else:
+                helper.append(char)
+            
+        return len(helper) == 0
+    
