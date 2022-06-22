751ac1337d074a21093dc646a7f135f303c9e1c8
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-06-21 23:30:01 -0400

    Time: 123 ms (21.91%), Space: 14 MB (99.77%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        occ = {}
+        
+        for i in range(len(magazine)):
+            occ[magazine[i]] = 1 + occ.get(magazine[i], 0)
+            
+        for char in ransomNote:
+            if char in occ and occ[char] > 0:
+                occ[char] = occ.get(char, 0) - 1
+            else:
+                return False
+            
+        return True
