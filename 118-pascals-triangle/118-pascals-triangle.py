d7ca3076a2b08e117574df3e4e05b0e481322540
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-06-21 23:59:23 -0400

    Time: 59 ms (16.31%), Space: 13.8 MB (65.62%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -0,0 +1,21 @@
+class Solution:
+    def generate(self, numRows: int):
+        
+        r = 1
+        index = 1
+        res = []
+        while r <= numRows:
+            new_list = [0] * r
+            new_list[0] = 1
+            new_list[r-1] = 1
+            
+            index = 1
+            while index < r-1:
+                new_list[index] = res[r-2][index-1] + res[r-2][index]
+                index += 1
+            
+            res.append(new_list)
+            r += 1
+        
+        return res
+    
