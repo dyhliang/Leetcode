a63be28000cc967a0e457ddb13c68e48ac8a61a2
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-06-21 23:39:53 -0400

    Time: 138 ms (65.50%), Space: 14.1 MB (58.12%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
@@ -1,6 +1,5 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        
         occ = {}   
         for char in s:
             occ[char] = 1 + occ.get(char, 0)
@@ -8,6 +7,5 @@ class Solution:
         for i in range(len(s)):
             if occ[s[i]] == 1:
                 return i
-            
         return -1
             
