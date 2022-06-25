d82841312fa47ac4ec6ce2d920794d665cc7d141
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-06-25 10:48:08 -0400

    Time: 48 ms (46.91%), Space: 13.8 MB (69.35%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -6,8 +6,6 @@ class Solution:
         helper = []
         for char in s:
             if char in pairs:
-                # if the stack is not empty and the top of the stack == the value to the key, pop
-                # else, return
                 if helper and (helper[-1] == pairs[char]):
                     top = helper.pop()
                 else:
