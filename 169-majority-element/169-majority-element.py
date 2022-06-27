56f8a82f150494c39d7b0d055e0201c883a59771
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-06-26 22:16:31 -0400

    Time: 193 ms (75.11%), Space: 15.5 MB (34.50%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,7 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         hash_t = {}
-        
         for val in nums:
             hash_t[val] = 1 + hash_t.get(val, 0) 
             
