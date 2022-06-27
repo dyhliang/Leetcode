25dc4c169efbda64cb1e98baeec6229d2b464502
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-06-26 20:31:43 -0400

    Time: 74 ms (99.98%), Space: 14.6 MB (98.64%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.py
+++ 566-reshape-the-matrix/566-reshape-the-matrix.py
@@ -2,14 +2,15 @@ class Solution:
     def matrixReshape(self, mat, r, c):
         res = []
         comb_lst = []
-        
         for lst in mat:
             comb_lst += lst
         
+        # pos: pos+c partitions each block from the comb_lst
         if r * c == len(comb_lst):
             for pos in range(0,len(comb_lst), c):
                 res.append(comb_lst[pos:pos+c])
-            return res
         else:
-            return mat
+            res = mat
+        
+        return res
     
