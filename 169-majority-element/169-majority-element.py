f83347234a5430b507cdbfc204094e286858cbae
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-06-26 22:16:04 -0400

    Time: 192 ms (75.78%), Space: 15.7 MB (34.50%) - LeetHub

---
+++ 169-majority-element/169-majority-element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        hash_t = {}
+        
+        for val in nums:
+            hash_t[val] = 1 + hash_t.get(val, 0) 
+            
+        for key in hash_t:
+            if hash_t[key] >= (len(nums) // 2 + 1):
+                return key
+            
