be3bb714ffe92734bc2e68ada0d3c9c07a39b302
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-06-26 22:32:06 -0400

    Time: 135 ms (49.33%), Space: 16.1 MB (11.55%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
@@ -1,4 +1,7 @@
 class Solution:
     def minPartitions(self, n: str) -> int:
+        # we just need the highest digit, therefore we would need at least that many
+        # 1's to build (digit * the place value of that digit)
         digits = [char for char in n]
         return max(digits)
+    
