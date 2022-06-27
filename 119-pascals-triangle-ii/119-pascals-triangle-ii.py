8eacd4123694120bf1112d2ffe7ac79725466572
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-06-26 21:05:57 -0400

    Time: 53 ms (31.98%), Space: 13.9 MB (60.31%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    
+    def generate(self, numRows: int) -> List[int]:
+        r = 1
+        index = 1
+        res = []
+        while r <= numRows:
+            new_list = [0] * r
+            new_list[0] = 1
+            new_list[r-1] = 1
+            index = 1
+            while index < r-1:
+                new_list[index] = res[r-2][index-1] + res[r-2][index]
+                index += 1
+            
+            res.append(new_list)
+            r += 1
+        
+        return res
+    
+    def getRow(self, rowIndex: int) -> List[int]:
+        rows = self.generate(rowIndex + 1)
+        return rows[-1]
