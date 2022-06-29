e7c85f3386fffbbcc25fc2400a5f4c03eb07b167
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-06-29 15:32:16 -0400

    Time: 132 ms (13.22%), Space: 18.1 MB (8.69%) - LeetHub

--- 243-shortest-word-distance/243-shortest-word-distance.py
+++ 243-shortest-word-distance/243-shortest-word-distance.py
@@ -2,13 +2,17 @@ class Solution:
     def shortestDistance(self, wordsDict: list[str], word1: str, word2: str) -> int:
         hash_t = {}
         diff_list = []
+        
+        # set up empty lists in hash table
         for pos in range(len(wordsDict)):
             hash_t[wordsDict[pos]] = []
 
+        # append indices for word1 and word2
         for pos in range(len(wordsDict)):
             if wordsDict[pos] == word1 or wordsDict[pos] == word2:
                 hash_t[wordsDict[pos]].append(pos)
 
+        # append all differences of indices between word1 and word2, then find the min
         for v1 in hash_t[word1]:
             for v2 in hash_t[word2]:
                 diff_list.append(abs(v2 - v1))
