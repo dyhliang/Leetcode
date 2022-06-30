3b4d63a8520d438aa43664a420a04c90f33bde6d
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-06-30 15:58:24 -0400

    Time: 88 ms (17.54%), Space: 14.2 MB (44.32%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.py
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -3,12 +3,13 @@ class Solution:
         mappings = {}
         
         for pos in range(len(s)):
+            # Map to hash table if neither character has been visited or used as a pair
             if s[pos] not in mappings.keys() and t[pos] not in mappings.values():
                 mappings[s[pos]] = t[pos]
             elif s[pos] not in mappings.keys() and t[pos] in mappings.values():
-                return False
+                return False    # if s char hasn't been used but t char has
             elif s[pos] in mappings.keys() and t[pos] != mappings[s[pos]]:
-                return False
+                return False    # is s char has been used but t char isn't its matched pair
             
         return True
     
