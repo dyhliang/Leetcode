50390bb0f105f961843ba225a110f619a4119478
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-06-29 22:41:52 -0400

    Time: 116 ms (48.02%), Space: 15.3 MB (79.18%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.py
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.py
@@ -2,7 +2,6 @@ class Solution:
     def minMoves2(self, nums: List[int]) -> int:
         nums.sort()
         modifier = nums[len(nums) // 2]
-        
         moves = 0
         
         for val in nums:
