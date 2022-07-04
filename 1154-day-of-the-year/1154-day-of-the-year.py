6b28b05e3c8a1c64ec50cf9eab63014c262d5132
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-07-03 22:24:15 -0400

    Time: 73 ms (94.59%), Space: 13.8 MB (77.80%) - LeetHub

--- 1154-day-of-the-year/1154-day-of-the-year.py
+++ 1154-day-of-the-year/1154-day-of-the-year.py
@@ -6,7 +6,7 @@ class Solution:
         day = int(date_list[2])
 
         # days accumulated by month, originally had 1: 31 and 12: 365 but this 
-        # should actually start at 2:31 because 31 days have past once Feb starts
+        # should actually start at 1:0 and 2:31 because 31 days have past once Feb starts
         days_accum = {
             1: 0,
             2: 31,
@@ -23,9 +23,10 @@ class Solution:
         }
         
         day_num = days_accum[month] + day
-        
+
+        # Filter at month == 3, don't do day_num > 59 or >= 60 because it adds an extra day already
         if month >= 3:
-            if year % 400 == 0:
+            if year % 400 == 0:  #1600 and 2000 are leap; 1700, 1800, 1900 are not
                 day_num += 1
             elif year % 4 == 0 and year % 100 != 0:
                 day_num += 1
