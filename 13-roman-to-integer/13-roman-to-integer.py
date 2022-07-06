38224aa520407556236d6fb9d9bf8b736da74e5a
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-07-06 12:12:25 -0400

    Time: 90 ms (24.87%), Space: 13.9 MB (30.71%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -2,18 +2,21 @@ class Solution:
     def romanToInt(self, s: str):
         vals = {"I": 1, "IV": 4, "V": 5, "IX": 9,
                 "X": 10, "XL": 40, "L": 50, "XC": 90,
-                "C": 100, "CD": 400, "D": 500, "CM": 900, "M": 1000}
+                "C": 100, "CD": 400, "D": 500, "CM": 900,
+                "M": 1000}
 
-        total = 0
+        digits = [char for char in s]
+        sum = 0
         pos = 0
 
-        while pos < len(s):
-            curr = vals[s[pos]]
-            if (pos < len(s) - 1) and (curr < vals[s[pos + 1]]):
-                total += vals[s[pos] + s[pos + 1]]
+        while pos < len(digits):
+            curr = vals[digits[pos]]
+
+            if (pos < len(digits) - 1) and (curr < vals[digits[pos + 1]]):
+                sum += vals[digits[pos] + digits[pos + 1]]
                 pos += 2
             else:
-                total += curr
+                sum += curr
                 pos += 1
 
-        return total
+        return sum
