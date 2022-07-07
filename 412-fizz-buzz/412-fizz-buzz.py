8226cf92d5670ff110301468df4a875d54b50a0c
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-07-07 14:22:40 -0400

    Time: 73 ms (36.71%), Space: 15 MB (43.26%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.py
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -1,7 +1,6 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         res = []
-        
         for i in range(1, n+1):
             if i % 15 == 0:
                 res.append("FizzBuzz")
