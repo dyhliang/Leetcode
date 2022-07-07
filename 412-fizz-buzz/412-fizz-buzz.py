c78e2a060ce352dc38e79db2dc122423cfcc7eff
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-07-07 14:29:51 -0400

    Time: 82 ms (19.34%), Space: 15.4 MB (4.77%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.py
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -1,15 +1,18 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         res = []
+        fizz_map = {3: "Fizz", 5: "Buzz"}
+        
         for i in range(1, n+1):
-            if i % 15 == 0:
-                res.append("FizzBuzz")
-            elif i % 5 == 0:
-                res.append("Buzz")
-            elif i % 3 == 0:
-                res.append("Fizz")
-            else:
-                res.append(str(i))
+            s = ""
+            for key in fizz_map.keys():
+                if i % key == 0:
+                    s += fizz_map[key]
+                    
+            if s == "":
+                s += str(i)
+            
+            res.append(s)
                 
         return res
     
