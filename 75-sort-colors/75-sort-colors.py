8c04a229f5726e4338a269ca656459a3c0ca60fb
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-07-09 13:49:34 -0400

    Time: 40 ms (81.09%), Space: 13.9 MB (63.64%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -1,6 +1,7 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         
+        #Bubble sort
         n = len(nums)
         for i in range(n):
             for j in range(0, n-i-1):
