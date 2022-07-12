7f3e02b1d9aba5bef845a2cf7a43bef376dca18a
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-07-11 23:34:28 -0400

    Time: 35 ms (80.07%), Space: 13.9 MB (9.46%) - LeetHub

---
+++ 1134-armstrong-number/1134-armstrong-number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isArmstrong(self, n: int) -> bool:
+        str_n = str(n)
+        digits_raised = [int(d) ** len(str_n) for d in str_n]
+        
+        return sum(digits_raised) == n
+                          
