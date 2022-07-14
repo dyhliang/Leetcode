00cd7a731d18cad8b70c1d714eb102efa8d80912
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-07-14 17:40:27 -0400

    Time: 124 ms (29.41%), Space: 15.3 MB (49.29%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.py
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.py
@@ -1,7 +1,9 @@
 class Solution:
     def largestOddNumber(self, num: str) -> str:
 
+        # Pointer starting from ones place
         for i in range(len(num)-1, -1, -1):
+            # Don't need to do [0:i+1] because we just need to test current digit is odd/even, helps with time complexity, return on the first digit to be odd
             if int(num[i]) % 2 == 1:
                 return num[0:i+1]
         
