96fb2f1f98b6db3f4496b75fa8a6b9ad23c105c7
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-07-15 21:33:55 -0400

    Time: 494 ms (59.67%), Space: 36 MB (11.85%) - LeetHub

---
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minimumAbsDifference(self, arr: list[int]) -> list[list[int]]:
+        arr.sort()  # sorts the arr to get adjacent values to be 'closer' to each other
+
+        min_diff = arr[1] - arr[0]
+        diff_pairs = []
+
+        for i in range(len(arr) - 1):
+            if arr[i + 1] - arr[i] < min_diff:
+                min_diff = arr[i + 1] - arr[i]
+
+            diff_pairs.append([arr[i], arr[i + 1]])
+
+        # check back to see which pairs have a difference == min diff
+        # Do this without second for loop?
+        res = [pair for pair in diff_pairs if pair[1] - pair[0] == min_diff]
+
+        return res
+    
