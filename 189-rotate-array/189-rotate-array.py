0dcf1a07625c0aca9bf42e19423bccc9ad82b8e4
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-07-18 17:07:52 -0400

    Time: 380 ms (43.43%), Space: 25.4 MB (27.59%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -10,4 +10,5 @@ class Solution:
         # for the subarray that needs to be swapped with the rest of the array
         new_nums[0:new_k] = nums[len(nums) - new_k: len(nums)]
         new_nums[new_k:] = nums[0:len(nums) - new_k]
-        nums[:] = new_nums
+        nums[:] = new_nums   # Need the [:] to actually dereference nums from original list
+        
