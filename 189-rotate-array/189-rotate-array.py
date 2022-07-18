6fa5f9243d09d6d84e95db798eeb7bcedbcde3dc
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-07-18 17:08:53 -0400

    Time: 322 ms (60.98%), Space: 25.4 MB (75.11%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -1,8 +1,5 @@
 class Solution:
     def rotate(self, nums: list[int], k: int) -> None:
-        if len(nums) == 0:
-            return []
-        
         new_nums = []
         new_k = k % len(nums)   # updates k to move minimally
 
