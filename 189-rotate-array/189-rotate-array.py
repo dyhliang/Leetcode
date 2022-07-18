8102adab9f18efd8da154954a0f81b85b14771bc
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-07-18 17:07:22 -0400

    Time: 432 ms (31.32%), Space: 25.3 MB (75.11%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -6,8 +6,8 @@ class Solution:
         new_nums = []
         new_k = k % len(nums)   # updates k to move minimally
 
-
-        # len(nums) - new_k for scenarios where 
+        # len(nums) - new_k marks the k to last index where that is the starting index
+        # for the subarray that needs to be swapped with the rest of the array
         new_nums[0:new_k] = nums[len(nums) - new_k: len(nums)]
         new_nums[new_k:] = nums[0:len(nums) - new_k]
         nums[:] = new_nums
