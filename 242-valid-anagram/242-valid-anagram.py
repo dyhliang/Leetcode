3e04fcf079b30a603f9a113bc9c709e863044860
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-07-28 09:19:38 -0400

    Time: 62 ms (73.44%), Space: 15.4 MB (11.53%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,5 +1,4 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        new_s = list(sorted(s))
-        new_t = list(sorted(t))
-        return new_s == new_t
+        return list(sorted(s)) == list(sorted(t))
+    
