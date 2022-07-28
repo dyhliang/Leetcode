795cfc1ed9716f25d6f41daeb79d32ed60b1e30b
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-07-28 09:17:21 -0400

    Time: 628 ms (35.01%), Space: 23.3 MB (81.46%) - LeetHub

---
+++ 398-random-pick-index/398-random-pick-index.py
@@ -0,0 +1,21 @@
+import random
+
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+        self.occs = {}
+        
+        for pos in range(len(nums)):
+            if nums[pos] not in self.occs.keys():
+                self.occs[nums[pos]] = [pos]
+            else:
+                self.occs[nums[pos]].append(pos)
+
+    def pick(self, target: int) -> int:
+        return random.choice(self.occs[target])
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.pick(target)
