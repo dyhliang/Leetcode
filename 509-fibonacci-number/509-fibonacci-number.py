0786ad646862069cbe21fe75767dc91b0a1edda3
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-08-02 18:11:56 -0400

    Time: 1097 ms (15.99%), Space: 13.8 MB (95.62%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,7 +1,6 @@
 class Solution:
     def fib(self, n: int) -> int:
-        
-        if n <= 1:
+        if n <= 1:      # Base case 
             return n
         
         return self.fib(n-1) + self.fib(n-2)
