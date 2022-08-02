fde1943d79babca3d0dd6a9af578352b82b4c973
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-08-02 18:11:43 -0400

    Time: 1170 ms (13.95%), Space: 14 MB (9.49%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,17 +1,7 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n < 2:
+        
+        if n <= 1:
             return n
-        else:
-            prevprev = 0
-            prev = 1
-            total = 1
-            curr = 2
-            
-            while curr < n:
-                prevprev = prev
-                prev = total
-                total += prevprev
-                curr += 1
-            
-            return total
+        
+        return self.fib(n-1) + self.fib(n-2)
