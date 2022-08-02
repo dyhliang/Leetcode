d4ec70bae726c4f38b6b8de91de9eefb153a65b4
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-08-02 18:22:05 -0400

    Time: 50 ms (55.55%), Space: 13.9 MB (9.86%) - LeetHub

--- 231-power-of-two/231-power-of-two.py
+++ 231-power-of-two/231-power-of-two.py
@@ -3,8 +3,11 @@ class Solution:
         if n == 0:
             return False
         
-        while n % 2 == 0:
-            n /= 2
-            
-        return n == 1
+        if n == 1:
+            return True
+        
+        if n % 2 == 0:
+            return self.isPowerOfTwo(int(n / 2))
+        else:
+            return False
     
