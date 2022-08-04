60444ef325e42c986c5239dd20efa98649839c3c
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-08-03 21:31:31 -0400

    Time: 65 ms (7.35%), Space: 13.9 MB (69.27%) - LeetHub

--- 1507-reformat-date/1507-reformat-date.py
+++ 1507-reformat-date/1507-reformat-date.py
@@ -3,7 +3,6 @@ class Solution:
         months_map = {"Jan": "01", "Feb": "02", "Mar": "03", "Apr": "04",
                       "May": "05", "Jun": "06", "Jul": "07", "Aug": "08",
                       "Sep": "09", "Oct": "10", "Nov": "11", "Dec": "12"}
-
         date_list = date.split(" ")     # [day, month, year]
         new_date = ""
         new_date += ((date_list[2] + "-") + (months_map[date_list[1]] + "-"))
