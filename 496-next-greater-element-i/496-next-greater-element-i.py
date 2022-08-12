9dbd3c1b3950339b0da77185876fda0702a81be8
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-08-11 22:06:45 -0400

    Time: 119 ms (26.78%), Space: 17.9 MB (14.92%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.py
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -2,9 +2,11 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         hash_t = {}
         res = []
+        # Store every element after current position into a list that maps to the current element as the key
         for i in range(len(nums2)):
             hash_t[nums2[i]] = nums2[i+1:]
 
+        # Can now use number from nums1 as index in dictionary, check each number's value list, break out when a greater value is found, if k gets to the end, there is no greater number
         for j in range(len(nums1)):
             if len(hash_t[nums1[j]]) == 0:
                 res.append(-1)
@@ -22,3 +24,4 @@ class Solution:
                     res.append(-1)
                     
         return res
+    
