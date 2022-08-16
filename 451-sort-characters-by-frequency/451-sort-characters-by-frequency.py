0faec19bb3e3d0abf73b451046b9442f32666389
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-08-16 15:02:10 -0400

    Update 451-sort-characters-by-frequency.py
    Added comments

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
@@ -4,8 +4,9 @@ class Solution:
         res = ""
 
         new_s = set(s)
-        # strip all duplicate letters to scan through long strings more efficiently
+        # strip all duplicate letters to scan through long strings faster
 
+        # store the occurrences of each letter as the key, the letter as the value 
         for char in new_s:
             if char not in occ.values():
                 if s.count(char) in occ.keys():
@@ -13,8 +14,10 @@ class Solution:
                 else:
                     occ[s.count(char)] = char
 
+        # Can't sort a dictionary, but we can use a separate list to sort its keys (which are ints)
         sorted_occ = sorted(occ.keys())
 
+        # Decreasing order, so iterate backwards from the new occurrences sorted list, and multiply each character for every occ key int by the key int
         for val in reversed(sorted_occ):
             for letter in occ[val]:
                 res += (val * letter)
