5948c860f9cca3be2d73f5fc77234775acd04358
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-08-18 13:11:23 -0400

    Time: 225 ms (20.73%), Space: 15.3 MB (34.81%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.py
+++ 229-majority-element-ii/229-majority-element-ii.py
@@ -1,6 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        new_nums = set(nums)
+        new_nums = set(nums)    # strip duplicates to iterate through more efficiently
         res = [val for val in new_nums if nums.count(val) > (len(nums) // 3)]
         return set(res)
     
