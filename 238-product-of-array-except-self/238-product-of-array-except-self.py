17611b3410ed1e62c6511ffdc213264e8d8504e6
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-08-21 20:08:26 -0400

    Time: 361 ms (49.98%), Space: 21.3 MB (49.53%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -11,11 +11,14 @@ class Solution:
         res = []
         for val in nums:
             if val == 0:
+            # when there's more than one zero, the product will always be 0
                 if nums.count(0) > 1:
                     res.append(0)
                 else:
-                    res.append(nonzero_prod)
+                    res.append(nonzero_prod)    
+                    # if there's 1 zero in the list, use nonzero_product
             else:
                 res.append(int(product * (val ** -1)))
+                # use current number raised to -1 as a workaround
 
         return res
