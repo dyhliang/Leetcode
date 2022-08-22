34f52ab201eaff17cb5e1638f38962422e77212e
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-08-22 11:29:24 -0400

    Time: 54 ms (34.83%), Space: 14 MB (11.52%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.py
+++ 771-jewels-and-stones/771-jewels-and-stones.py
@@ -1,7 +1,9 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        
         jewels_list = list(jewels)
         res = 0
+        
         for item in jewels_list:
             res += stones.count(item)
             
