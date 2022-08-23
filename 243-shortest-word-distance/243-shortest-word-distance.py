b854cea45c27af3fb4c03c81b3f44e13575d74a1
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-08-23 00:57:15 -0400

    Time: 148 ms (6.07%), Space: 17.8 MB (47.95%) - LeetHub

--- 243-shortest-word-distance/243-shortest-word-distance.py
+++ 243-shortest-word-distance/243-shortest-word-distance.py
@@ -2,6 +2,7 @@ class Solution:
     def shortestDistance(self, wordsDict: list[str], word1: str, word2: str) -> int:
         hash_t = {}
 
+        # Fill in hash table with indices for word1 and word2
         for i, w in enumerate(wordsDict):
             if w == word1 or w == word2:
                 if w not in hash_t.keys():
@@ -9,6 +10,8 @@ class Solution:
                 else:
                     hash_t[w].append(i)
 
+        # res used to find all combinations of index pairings between the two words
+        # find the min difference
         res = [abs(a - b) for a in hash_t[word1] for b in hash_t[word2]]
         return min(res)
     
