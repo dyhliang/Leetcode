8ab01a2c2bd0f062c511ff3bec0cf78cb03df5e5
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-08-22 23:30:03 -0400

    Time: 226 ms (39.59%), Space: 14.4 MB (95.80%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.py
+++ 1603-design-parking-system/1603-design-parking-system.py
@@ -2,7 +2,7 @@ class ParkingSystem:
 
     def __init__(self, big: int, medium: int, small: int):
         self.tracking = {
-            # [spaces available, spaces parked]
+            # map the carType ranking to each available space
             1: big,
             2: medium,
             3: small,
@@ -10,8 +10,8 @@ class ParkingSystem:
 
     def addCar(self, carType: int) -> bool:
         self.tracking[carType] -= 1
+        # Always decrements by 1 and checks if the corresponding carType's space is not negative
         return self.tracking[carType] >= 0
-
 
 # Your ParkingSystem object will be instantiated and called as such:
 # obj = ParkingSystem(big, medium, small)
