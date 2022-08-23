acd53860aafd039cdc50bd96036cee5d14031b30
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-08-23 01:13:51 -0400

    Time: 59 ms (97.97%), Space: 15.1 MB (50.22%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -3,8 +3,10 @@ class Solution:
         prev = {}
         
         for pos, n in enumerate(nums):
+            # We already know what the target is, so subtraction gets the other number
             diff = target - n
             if diff in prev:
                 return [prev[diff], pos]
             
             prev[n] = pos
+            # store the index as the value to the key of the other number
