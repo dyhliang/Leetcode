603b3fec320e7444b2b8ebe3af0f1d69141e4984
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-08-22 23:28:43 -0400

    Time: 271 ms (12.66%), Space: 14.3 MB (95.80%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.py
+++ 1603-design-parking-system/1603-design-parking-system.py
@@ -3,14 +3,14 @@ class ParkingSystem:
     def __init__(self, big: int, medium: int, small: int):
         self.tracking = {
             # [spaces available, spaces parked]
-            1: [big, 0],
-            2: [medium, 0],
-            3: [small, 0]
+            1: big,
+            2: medium,
+            3: small,
         }
 
     def addCar(self, carType: int) -> bool:
-        self.tracking[carType][1] += 1
-        return self.tracking[carType][1] <= self.tracking[carType][0]
+        self.tracking[carType] -= 1
+        return self.tracking[carType] >= 0
 
 
 # Your ParkingSystem object will be instantiated and called as such:
