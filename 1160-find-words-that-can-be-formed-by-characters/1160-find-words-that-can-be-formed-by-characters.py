f5d1b2197fa4035403cfed90e2918c2ad98663a1
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-08-25 14:10:47 -0400

    Time: 503 ms (12.10%), Space: 14.6 MB (6.73%) - LeetHub

---
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
@@ -0,0 +1,24 @@
+from copy import deepcopy
+
+
+class Solution:
+    def countCharacters(self, words: list[str], chars: str) -> int:
+        occ = {}
+        for c in chars:
+            occ[c] = 1 + occ.get(c, 0)
+        res = []
+
+        for word in words:
+            temp = deepcopy(occ)
+            for pos, letter in enumerate(word):
+                if letter in occ.keys() and occ[letter] > 0:
+                    occ[letter] = occ.get(letter, 0) - 1
+
+                    if pos == len(word) - 1:
+                        res.append(len(word))
+                else:
+                    break
+
+            occ = temp
+
+        return sum(res)
