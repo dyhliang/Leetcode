ecc875b28e025de10ae3617ddd79436b4e4cb552
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-08-27 22:55:33 -0400

    Time: 52 ms (32.66%), Space: 13.9 MB (6.98%) - LeetHub

---
+++ 319-bulb-switcher/319-bulb-switcher.py
@@ -0,0 +1,5 @@
+class Solution:
+    def bulbSwitch(self, n: int) -> int:
+        return int(sqrt(n))
+    
+    # Proof: https://leetcode.com/problems/bulb-switcher/discuss/77112/Share-my-o(1)-solution-with-explanation 
