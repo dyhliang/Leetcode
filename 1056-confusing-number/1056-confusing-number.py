0f39a21a4c7770098fe758528c3ebbc690da78c4
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-08-27 23:43:39 -0400

    Time: 55 ms (25.00%), Space: 13.9 MB (12.50%) - LeetHub

---
+++ 1056-confusing-number/1056-confusing-number.py
@@ -0,0 +1,18 @@
+class Solution:
+    def confusingNumber(self, n: int) -> bool:
+        flip = {"0": "0",
+                "1": "1",
+               "6": "9",
+               "8": "8",
+               "9": "6"}
+        
+        new_n = ""
+        
+        for char in str(n)[::-1]:
+            if char not in flip.keys():
+                return False
+            else:
+                new_n += flip[char]
+                
+        return int(new_n) != n
+    
