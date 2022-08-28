f9c2ccb2f92762077ffc13d0256607bb29d3b972
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-08-27 23:47:27 -0400

    Time: 44 ms (59.29%), Space: 13.9 MB (65.06%) - LeetHub

--- 1056-confusing-number/1056-confusing-number.py
+++ 1056-confusing-number/1056-confusing-number.py
@@ -5,13 +5,18 @@ class Solution:
                "6": "9",
                "8": "8",
                "9": "6"}
-        new_n = ""
-        rev_str = str(n)[::-1]
-        for char in rev_str:
-            if char not in flip.keys():
-                return False
-            else:
-                new_n += flip[char]
-                
-        return int(new_n) != n
-    
+        
+        if any(digit in ("2", "3", "4", "5", "7") for digit in str(n)):
+            return False
+        else:
+            new_n = ""
+            rev_str = str(n)[::-1]
+
+
+            for digit in rev_str:
+                if digit not in flip.keys():
+                    return False
+                else:
+                    new_n += flip[digit]
+
+            return int(new_n) != n
