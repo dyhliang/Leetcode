085b37b919e7de1fa4a85cc6950b624fe34d5328
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-08-30 18:03:54 -0400

    Time: 64 ms (33.01%), Space: 13.9 MB (31.85%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -4,8 +4,9 @@ class Solution:
         t_list = [*t]
         s_list.sort()
         t_list.sort()
-
         
+        # Sort the letters inside the two strings
+        # If the pointer gets past the last position of s, return the value at that pos in t, this if statement goes first so the ensuing if statement won't get an out of bounds error comparing a character at a position that doesn't exist.
         for pos in range(max(len(s_list), len(t_list))):
             if pos >= len(s_list):
                 return t_list[pos]
