604d00227d3ec0cf1159a3686398b1509dcc9c5d
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-08-30 18:05:43 -0400

    Time: 72 ms (19.27%), Space: 13.9 MB (76.19%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,16 +1,14 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        s_list = [*s]
-        t_list = [*t]
-        s_list.sort()
-        t_list.sort()
+        s_sort = sorted(s)
+        t_sort = sorted(t)
         
         # Sort the letters inside the two strings
         # If the pointer gets past the last position of s, return the value at that pos in t, this if statement goes first so the ensuing if statement won't get an out of bounds error comparing a character at a position that doesn't exist.
-        for pos in range(max(len(s_list), len(t_list))):
-            if pos >= len(s_list):
-                return t_list[pos]
+        for pos in range(max(len(s), len(t))):
+            if pos >= len(s_sort):
+                return t_sort[pos]
             
-            if s_list[pos] != t_list[pos]:
-                return t_list[pos]
+            if s_sort[pos] != t_sort[pos]:
+                return t_sort[pos]
             
