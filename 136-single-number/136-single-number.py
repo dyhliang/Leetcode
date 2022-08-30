599098a8331f1b5341ef50c1f672ba32ae81d6c3
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-08-30 17:48:56 -0400

    Time: 6858 ms (7.44%), Space: 16.2 MB (22.20%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,10 +1,9 @@
 class Solution(object):
     def singleNumber(self, nums):
-        occ = {}
-        for val in nums:
-            occ[val] = 1 + occ.get(val, 0)
-            
-        for key in occ:
-            if occ[key] == 1:
-                return key
+
+        no_dupes = set(nums)
+    
+        for val in no_dupes:
+            if nums.count(val) == 1:
+                return val
         
