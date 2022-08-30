0d83986a1c6c4201c3773ac6d1f27ebc42aa5653
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-08-30 18:01:34 -0400

    Time: 60 ms (41.71%), Space: 14 MB (31.85%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        s_list = [*s]
+        t_list = [*t]
+        s_list.sort()
+        t_list.sort()
+
+        for pos in range(max(len(s_list), len(t_list))):
+            if pos >= len(s_list):
+                return t_list[pos]
+            
+            if s_list[pos] != t_list[pos]:
+                return t_list[pos]
+            
