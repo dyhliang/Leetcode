1f15cff14f66e2e23338c8c2e6470cf06fa28461
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-08-31 00:48:30 -0400

    Time: 120 ms (83.81%), Space: 18.7 MB (71.71%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -2,17 +2,21 @@ import heapq
 
 class Solution:
     def topKFrequent(self, nums: list[int], k: int):
+        occ = {}
         max_heap = []
         no_dupes = set(nums)
-        
+
+        for n in nums:
+            occ[n] = 1 + occ.get(n, 0)
+
         for val in no_dupes:
-            heapq.heappush(max_heap, ((-1 * nums.count(val)), val))
-            
+            heapq.heappush(max_heap, ((-1 * occ[val]), val))
+
         res = []
         while k > 0:
             n = heapq.heappop(max_heap)
             res.append(n[1])
             k -= 1
-            
+
         return res
     
