8eae60974306bd6ac801e8a2b89de49987126b3a
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-08-31 00:07:04 -0400

    Time: 54 ms (97.83%), Space: 14 MB (26.18%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.py
+++ 692-top-k-frequent-words/692-top-k-frequent-words.py
@@ -4,17 +4,17 @@ class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         max_heap = []
         no_dupes = set(words)
+        
         for word in no_dupes:
             # Creates max_heap with elements inside the list as tuples (-1 * occ, word)
             # heapppush also reduces the need to sort the occs outside of this loop.
             heapq.heappush(max_heap, ((-1 * words.count(word)), word))
 
-        count = 0
         res = []
-        while count < k:
-            count += 1
+        while k > 0:
             w = heapq.heappop(max_heap)
             res.append(w[1])
+            k -= 1
 
         return res
     
