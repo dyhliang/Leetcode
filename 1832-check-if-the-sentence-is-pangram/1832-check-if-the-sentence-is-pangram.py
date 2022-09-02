ee36dbd5e3a0c42fb54622e174c4944c12b4e66f
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-09-02 10:13:30 -0400

    Time: 52 ms (42.69%), Space: 13.9 MB (10.82%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -1,11 +1,10 @@
 class Solution:
     def checkIfPangram(self, s: str) -> bool:
-        alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    
-        lower_s = s.lower()
-        for letter in alpha:
-            if letter not in lower_s:
-                return False
+        no_dupes = "".join(set(str(s)))
+        occ = {}
+        
+        for char in no_dupes:
+            occ[char] = 1 + occ.get(char, 0)
 
-        return True
+        return len(occ.keys()) == 26
     
