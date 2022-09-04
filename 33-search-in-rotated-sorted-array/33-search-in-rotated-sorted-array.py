0ca7dadba37435bc69730986976034b84052a50f
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-09-04 17:24:58 -0400

    Time: 46 ms (88.93%), Space: 14.2 MB (90.92%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if target not in nums:
+            return -1
+        else:
+            return nums.index(target)
+        
