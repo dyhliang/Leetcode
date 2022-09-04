edf987195eaf1bb6870eb041ffad817aec31151c
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-09-03 21:04:16 -0400

    Time: 108 ms (97.69%), Space: 14.1 MB (57.38%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
@@ -1,23 +1,22 @@
 class Solution:
-    def diagonalSum(self, mat: List[List[int]]) -> int:
+    def diagonalSum(self, arr: list[list[int]]) -> int:
         left = 0
-        right = len(mat[0]) - 1
+        right = len(arr[0]) - 1
         total = 0
-        
-        for arr in mat:
-            for i in range(len(arr)):
-                
-                if i == left == right:
-                    total += arr[i]
-                else:
-                    if i == left:
-                        total += arr[i]
 
-                    if i == right:
-                        total += arr[i]
+        for i in range(len(arr)):
+
+            if i == (len(arr) - i - 1):
+                total += arr[i][i]
+            else:
+                if i == left:
+                    total += arr[i][i]
+
+                if (len(arr) - i - 1) == right:
+                    total += arr[i][len(arr) - i - 1]
 
             left += 1
             right -= 1
-        
+
+
         return total
-    
