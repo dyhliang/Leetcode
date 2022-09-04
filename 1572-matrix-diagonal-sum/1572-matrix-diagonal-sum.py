4e9d0de17cc428d807fb97493ae9ed99fac19ea4
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-09-03 21:12:37 -0400

    Time: 215 ms (18.70%), Space: 14.1 MB (57.38%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
@@ -1,7 +1,5 @@
 class Solution:
     def diagonalSum(self, arr: list[list[int]]) -> int:
-        left = 0
-        right = len(arr[0]) - 1
         total = 0
 
         for i in range(len(arr)):
@@ -9,14 +7,6 @@ class Solution:
             if i == (len(arr) - i - 1):
                 total += arr[i][i]
             else:
-                if i == left:
-                    total += arr[i][i]
-
-                if (len(arr) - i - 1) == right:
-                    total += arr[i][len(arr) - i - 1]
-
-            left += 1
-            right -= 1
-
+                total += (arr[i][i] + arr[i][len(arr) - i - 1])
 
         return total
