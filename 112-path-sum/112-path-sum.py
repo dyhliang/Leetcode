5ee7f0bbd2faae54650361170788562efc00ae0b
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-09-04 15:46:42 -0400

    Time: 87 ms (14.56%), Space: 15.2 MB (15.86%) - LeetHub

--- 112-path-sum/112-path-sum.py
+++ 112-path-sum/112-path-sum.py
@@ -14,6 +14,7 @@ class Solution:
         self.traverse(root.left, arr, total)
         self.traverse(root.right, arr, total)
         
+        # Works like postorder traversal
         if not root.left and not root.right:
             arr.append(total)
             
