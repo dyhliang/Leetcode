42e0df687db01ab778342d216a9fed4e2c5c0029
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-09-05 20:55:59 -0400

    Time: 89 ms (73.35%), Space: 15.3 MB (23.95%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -2,12 +2,10 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
         seen = {}
-        res = []
         for i, n in enumerate(nums):
             diff = target - n
             if diff not in seen.keys():
                 seen[n] = i
             else:
                 res = [seen[diff], i]
-                
-        return res
+                return res
