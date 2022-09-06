31bcb2a41742716394c910774769fbbdb30046d0
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-09-05 20:22:35 -0400

    Time: 93 ms (26.48%), Space: 14.7 MB (40.58%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -6,10 +6,10 @@ class Solution:
         # Use Binary Search to look for the index as the array is already sorted.
         while left <= right:
             middle = (left + right) // 2
-            
             if nums[middle] == target:
                 return middle
-            elif nums[middle] < target:
+            
+            if nums[middle] < target:
                 left = middle + 1
             else:
                 right = middle - 1
