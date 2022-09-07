36daa063febf040923729dbb3c7a584051921596
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-09-07 17:17:47 -0400

    Time: 1781 ms (5.89%), Space: 14.1 MB (33.46%) - LeetHub

---
+++ 264-ugly-number-ii/264-ugly-number-ii.py
@@ -0,0 +1,29 @@
+import heapq
+
+class Solution:
+    
+    def isUgly(self, n: int, seen: set, h: list) -> bool:
+        for p in [2, 3, 5]:
+            new_ugly = n * p
+            if new_ugly not in seen:
+                seen.add(new_ugly)
+                heapq.heappush(h, new_ugly)
+                
+    
+    def nthUglyNumber(self, n: int) -> int:
+        
+        seen = {1, }
+        nums = []
+        heap = []
+        heapq.heappush(heap, 1)
+        
+        
+        for val in range(1690):
+            curr = heapq.heappop(heap)
+            nums.append(curr)
+            self.isUgly(curr, seen, heap)
+            
+        return nums[n-1]
+            
+            
+    
