666100807b90ca1d0a20f327f946ba33cf236b4e
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-09-07 20:28:11 -0400

    Time: 89 ms (72.63%), Space: 13.9 MB (86.37%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -17,16 +17,16 @@ class Solution:
             sec_lst.append(str(curr.val))
             curr = curr.next
             
-        num_1 = int(''.join(first_lst[::-1]))
-        num_2 = int(''.join(sec_lst[::-1]))
+        num_1 = int(''.join(first_lst)[::-1])
+        num_2 = int(''.join(sec_lst)[::-1])
         
         total = str(num_1 + num_2)[::-1]
         
-        head = ListNode(0)
-        curr = head
+        sentinel = ListNode(0)
+        curr = sentinel
         for digit in total:
             curr.next = ListNode(int(digit))
             curr = curr.next
             
-        return head.next
+        return sentinel.next
     
