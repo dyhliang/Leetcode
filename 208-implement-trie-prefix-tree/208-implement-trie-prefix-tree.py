e7120c9161d2f150cefe294516a2a41869561f03
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-09-10 22:21:52 -0400

    Time: 129 ms (98.80%), Space: 27.5 MB (87.21%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
@@ -4,24 +4,33 @@ class Trie:
         self.trie = {}
 
     def insert(self, word: str) -> None:
+        # t is a pointer that constantly update to point to the children of the current character, if any
         t = self.trie
         for char in word:
             if char not in t:
+                # if character is not found, add a new key entry
                 t[char] = {}
+            
+            # regardless of whether char is found, t now points to the 
+            # children of the current char
             t = t[char]
                 
-        t["+"] = True
+        # The key entry here can be any string value that is not an alphabetical letter preferably, use True to indicate that the insertion is complete.
+        t["√"] = True
 
     def search(self, word: str) -> bool:
+        # Same steps as insert to start off
         t = self.trie
         for char in word:
+            # Main difference is if the char is not found, return False to break the loop
             if char not in t:
                 return False
             
             t = t[char]
             
-        return "+" in t
-        #return self.startsWith(word)
+        # Checks for the completion status for the word if it was fully inserted. 
+        return "√" in t
+
 
     def startsWith(self, prefix: str) -> bool:
         t = self.trie
@@ -30,7 +39,8 @@ class Trie:
                 return False
         
             t = t[char]
-            
+        
+        # Simply return true if we're able to iterate through without returning False for char not found in t.
         return True
 
 
