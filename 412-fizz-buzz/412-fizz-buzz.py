07276bd2e2b542e81799d6ace346a4fdf5500462
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-09-12 22:39:13 -0400

    Time: 81 ms (26.37%), Space: 15.2 MB (17.90%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.py
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -1,18 +1,8 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         res = []
-        fizz_map = {3: "Fizz", 5: "Buzz"}
-        
         for i in range(1, n+1):
-            s = ""      # resets the string for every iteration from 1 to n
-            for key in fizz_map.keys():
-                if i % key == 0:
-                    s += fizz_map[key]  # Won't need to check for divisiblity by 15
-                    
-            if s == "": # If number is not divisible by 3 or 5, s will be empty
-                s += str(i)
-            
-            res.append(s)
-                
+            res.append(((i % 3 == 0)*"Fizz" + (i % 5 == 0)*"Buzz" or str(i)))
+                       
         return res
     
