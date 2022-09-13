0a7e8a34584fb2a0dfd1ba009ed91865a2e832a2
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-09-13 12:22:25 -0400

    Time: 132 ms (26.26%), Space: 13.9 MB (43.23%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -5,6 +5,7 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # Iterate through l1 and l2 and append its digits casted as a str onto its own list
         curr = l1
         first_lst = []
         while curr:
@@ -17,12 +18,15 @@ class Solution:
             sec_lst.append(str(curr.val))
             curr = curr.next
             
+        # Join and reverse the digits for both lists, then cast into an int so we can 
+        # use the normal + operation on ints to forgo using extra ifs or loops to carry
         num_1 = int(''.join(first_lst)[::-1])
         num_2 = int(''.join(sec_lst)[::-1])
         
+        # Reverse the sum back since we want to start with ones digit in the LL
+        # use a front sentinel and curr to make pointer assignments less confusing
         total = str(num_1 + num_2)[::-1]
-        
-        sentinel = ListNode(0)
+        sentinel = ListNode(0)  # The extra 0 here won't be an issue as we can return sentinel.next to avoid it
         curr = sentinel
         for digit in total:
             curr.next = ListNode(int(digit))
