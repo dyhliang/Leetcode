177704317cc7f24dd609d871312c9a131fea9a66
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-09-12 22:48:22 -0400

    Time: 49 ms (86.01%), Space: 15.3 MB (17.90%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.py
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -5,9 +5,11 @@ class Solution:
         for i in range(1, n+1):
             string = ""
             for key in hasht.keys():
+                # Reduces the need for multiple ifs to test 3, 5, and 15
                 if i % key == 0:
                     string += hasht[key]
             
+            # If the string wasn't modified, then i is not divisible by 3, 5, and 15
             if string == "":
                 string = str(i)
 
