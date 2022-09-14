4a10dbf88644f8a6f3b2d720fc029abd05a38ddb
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-09-13 22:17:14 -0400

    Time: 71 ms (33.03%), Space: 14.3 MB (15.26%) - LeetHub

--- 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.py
+++ 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.py
@@ -6,6 +6,7 @@
 
 class Solution:
     def deleteNode(self, node):
+        
         # Since we don't have access to the prev node, we need to modify the current node's
         # value in place to the value of its next node, and its next pointer so that it's pointing to the node 2 spots over to its right.
         node.val = node.next.val
