f7f1edfc3c79daf7cc32d5a2d994c3d5eefd1a66
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-09-15 18:07:11 -0400

    Time: 458 ms (26.97%), Space: 14.4 MB (99.42%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,11 +1,10 @@
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
         s_hash = {}
-        
+        count = 0
         for char in s:
             s_hash[char] = 1 + s_hash.get(char, 0)
             
-        count = 0
         for char in t:
             if char not in s_hash.keys() or s_hash[char] <= 0:
                 count += 1
