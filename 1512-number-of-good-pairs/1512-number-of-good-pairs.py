c5b62baf1b2521e5d684742f0f19dd102c43ee02
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-09-14 23:28:04 -0400

    Time: 51 ms (58.76%), Space: 13.9 MB (56.16%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -2,6 +2,7 @@ import math
 
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
+        # Use a hash table to map the index of each value to the val
         hashmap = {}
         for pos, val in enumerate(nums):
             if val not in hashmap.keys():
@@ -9,11 +10,16 @@ class Solution:
             else:
                 hashmap[val].append(pos)
         
+        # This is a combinations problem, where order doesn't matter based on Ex. 1 - (3, 0) is not mentioned with (0, 3)
         pairs = 0
         for key in hashmap.keys():
-            l = len(hashmap[key])
-            if l > 1:
-                pairs += math.factorial(l)/(math.factorial(2) * math.factorial(l - 2))
+            n = len(hashmap[key])
+            # Combinations are only possible if there's 2 or more numbers to work with
+            if n > 1:
+                # Use the combinations formula where n is the length of numbers in value arr
+                # r is 2
+                # n!/r!(n-r)!
+                pairs += math.factorial(n)/(math.factorial(2) * math.factorial(n - 2))
                 
         return int(pairs)
     
