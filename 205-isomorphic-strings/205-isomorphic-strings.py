3e7c3afd670a574a7f3269941ff20ae44d0852e8
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-09-20 13:23:51 -0400

    Time: 104 ms (12.80%), Space: 14.2 MB (88.59%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.py
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -1,13 +1,13 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         mappings = {}
-        for pos in range(len(s)):
+        for i, char in enumerate(s):
             # Map to hash table if neither character has been visited or used as a pair
-            if s[pos] not in mappings.keys() and t[pos] not in mappings.values():
-                mappings[s[pos]] = t[pos]
-            elif s[pos] not in mappings.keys() and t[pos] in mappings.values():
+            if s[i] not in mappings.keys() and t[i] not in mappings.values():
+                mappings[s[i]] = t[i]
+            elif s[i] not in mappings.keys() and t[i] in mappings.values():
                 return False    # if s char hasn't been used but t char has
-            elif s[pos] in mappings.keys() and t[pos] != mappings[s[pos]]:
+            elif s[i] in mappings.keys() and t[i] != mappings[s[i]]:
                 return False    # is s char has been used but t char isn't its matched pair
             
         return True
