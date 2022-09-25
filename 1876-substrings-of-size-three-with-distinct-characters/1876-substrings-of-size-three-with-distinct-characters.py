6f1a10b0be78dee1b45fa65ecc07b46c162b0d9d
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-09-25 14:05:13 -0400

    Time: 82 ms (5.13%), Space: 13.8 MB (95.24%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.py
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.py
@@ -1,11 +1,22 @@
+from collections import deque
+
 class Solution:
-    def countGoodSubstrings(self, s: str) -> int:
-        res = []
+    def countGoodSubstrings(self, s: str) -> int:        
+        window = deque([])
+        count = 0
+
+        for i, char in enumerate(s):
+            if len(window) > 0:
+                if window[-1] == char:
+                    window = deque([])
+            
+            if len(window) > 2:
+                window.popleft()
+                
+            if char not in window and len(window) == 2:
+                count += 1
+                
+            window.append(char)
         
-        for i, trio in enumerate(s[0:len(s)-2]):
-            trio = s[i:i+3]
-            if len(set(trio)) == len(trio):
-                res.append(trio)
-        
-        return len(res)
+        return count
     
