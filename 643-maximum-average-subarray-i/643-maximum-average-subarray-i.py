d58fcd7066f82a2c130ff69682ac2186f1e69e14
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-10-09 00:21:28 -0400

    Time: 3433 ms (9.94%), Space: 25.9 MB (52.15%) - LeetHub

--- 643-maximum-average-subarray-i/643-maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/643-maximum-average-subarray-i.py
@@ -4,6 +4,8 @@ class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         window = deque([])
         max_avg = sum(nums[0:k]) / k
+        
+        # Running sum() in the for loop results in TLE, have to keep track of running sum to get under it
         curr_avg = max_avg
         curr_sum = 0
         
