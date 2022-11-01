54df851537b96c846271565ddea09ea25a215770
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-11-01 17:58:36 -0400

    Time: 705 ms (23.44%), Space: 16.6 MB (8.28%) - LeetHub

--- 697-degree-of-an-array/697-degree-of-an-array.py
+++ 697-degree-of-an-array/697-degree-of-an-array.py
@@ -1,5 +1,24 @@
 class Solution:
     def findShortestSubArray(self, nums: List[int]) -> int:
+        """
+        Understand
+            Degree of an array is the max freq of any one of its elements
+            Array is guaranteed to be non-empty
+            Integers are always positive
+
+        Edge cases:
+            Nums arr has: 1 element
+
+
+        Match
+            Hash table
+            2 for loops: 1 to set up hash table of indices occurrences, 1 to find the               first indices occurrence array that is the same length value as the                     max_freq
+
+        Plan
+            Use hash table to keep track of indices for each unique element in array,               while also keeping tracking of the max freq element
+
+            Shortest length will come from the difference of index -1 and 0 for the                 first indices occurrence array that has the same length as max_freq
+        """
         hmap = {}
         max_freq = 0
 
