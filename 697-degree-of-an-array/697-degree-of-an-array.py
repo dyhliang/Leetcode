f60ae207404b84efc49a6625ff8750094fc9ce7d
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-11-01 17:53:45 -0400

    Time: 633 ms (34.73%), Space: 16.6 MB (16.77%) - LeetHub

---
+++ 697-degree-of-an-array/697-degree-of-an-array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findShortestSubArray(self, nums: List[int]) -> int:
+        hmap = {}
+        max_freq = 0
+
+        for i, val in enumerate(nums):
+            if val not in hmap.keys():
+                hmap[val] = [i]
+            else:
+                hmap[val].append(i)
+
+            max_freq = max(max_freq, len(hmap[val]))
+
+        shortest_len = float('inf')
+        for arr in hmap.values():
+            if len(arr) == max_freq:
+                curr_len = arr[-1] - arr[0] + 1
+                shortest_len = min(shortest_len, curr_len)
+
+        return shortest_len
