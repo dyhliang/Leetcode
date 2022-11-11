ae1a05d717967a701f897ec03cf60783f1770e28
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-11-10 23:28:02 -0500

    Time: 736 ms (18.03%), Space: 15.5 MB (33.86%) - LeetHub

--- 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.py
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.py
@@ -1,10 +1,11 @@
 class Solution:
     def minMoves(self, nums: List[int]) -> int:
         
-        # [1,1,1] = 0, every value is the same
-        # [1,2,7] = 
-        
         nums.sort()
+        # This strategy only works if the array is sorted
+        # Take the subarray from index 1 onwards, find its sum
+        # Use that sum to subtract (the value at index 0 x the length of that subarray)
         total = sum(nums[1:])
         res = total - (nums[0] * len(nums[1:]))
         return res
+    
