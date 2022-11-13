4cbf2a36933eaad14cb03da1a7c8d2a37514606b
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-11-12 23:55:06 -0500

    Time: 847 ms (23.11%), Space: 16.6 MB (75.36%) - LeetHub

---
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        
+        res = 0
+        pref_sum = 0
+        table = {0: 1}
+        
+        for val in nums:
+            pref_sum += val
+            
+            if pref_sum - k in table:
+                res += table[pref_sum - k]
+            
+            if pref_sum not in table:
+                table[pref_sum] = 1
+            else:
+                table[pref_sum] += 1
+                
+        return res
+            
