2eb33891e0c03738bc5aac1150727bc8008e53cc
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2022-11-30 12:36:58 -0500

    Time: 84 ms (12.15%), Space: 14.1 MB (34.11%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,16 +1,8 @@
 class Solution:
-    def uniqueOccurrences(self, arr):
-        
-        # use hash table to store occurrences, adjust index if collision
-        occ = {}
-        for i in range(len(arr)):
-            if arr[i] < 0:
-                occ[arr[i] * -7] = 1 + occ.get(arr[i] * -7, 0) 
-            else:
-                occ[arr[i]] = 1 + occ.get(arr[i], 0) 
-        
-        # convert occurrences values to a set to weed out duplicates
-        occ_keys = occ.keys()
-        occ_vals = occ.values()
-        return len(occ_keys) == len(set(occ_vals))
-    
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        d = {}
+        for val in arr:
+            d[val] = 1 + d.get(val, 0)
+            
+        return len(d.values()) == len(set(d.values()))
+            
