f10ffd58f714e7df52a2f215677e99e619778559
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2023-01-01 12:23:08 -0500

    Time: 61 ms (73.38%), Space: 14.8 MB (79.40%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.py
+++ 414-third-maximum-number/414-third-maximum-number.py
@@ -2,19 +2,14 @@ import heapq as hq
 
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
-        maxheap = []
-        res = 0
-
-        for val in nums:
-            if -val not in maxheap:
-                hq.heappush(maxheap, -val)
-
-        if len(maxheap) < 3:
-            res = -1 * hq.heappop(maxheap)
-        else:
-            i = 0
-            while i < 3:
-                res = -1 * hq.heappop(maxheap)
-                i += 1
-
-        return res
+        desc = sorted(nums)[::-1]
+        seen = []
+        
+        for val in desc:
+            if val not in seen:
+                seen.append(val)
+                
+            if len(seen) == 3:
+                return seen[-1]
+            
+        return seen[0]
