1ba1dfd0c9b1770f7ae5cda1a8a50b12976dd9e3
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2023-01-01 12:58:40 -0500

    Time: 101 ms (90.08%), Space: 17.2 MB (79.29%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,18 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        dict = {}
+        res = []
+        worddict = {}
+        
         for word in strs:
-            # Strings are immutable, so split the letters into a list, then sort lexicographically
-            # to form a new word that serves as the key to hold all other anagrams to the key (if any)
-            word_list = [*word]
-            word_list.sort()
-            lex_word = "".join(word_list)
-
-            if lex_word not in dict.keys():
-                dict[lex_word] = [word]
+            conv_word = ''.join(sorted([*word]))
+            if conv_word not in worddict.keys():
+                worddict[conv_word] = [word]
             else:
-                dict[lex_word].append(word)
-
-        res = [li for li in dict.values()]
+                worddict[conv_word].append(word)
+                
+        for w in worddict.values():
+            res.append(w)
+            
         return res
     
