4f316fe1c8eb23b28a6bb8e08d1bc4153fd1110b
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2023-01-03 14:38:08 -0500

    Time: 121 ms (92.73%), Space: 15.3 MB (6.95%) - LeetHub

--- 944-delete-columns-to-make-sorted/944-delete-columns-to-make-sorted.py
+++ 944-delete-columns-to-make-sorted/944-delete-columns-to-make-sorted.py
@@ -3,12 +3,12 @@ class Solution:
         # Use list comp to combine letters at each str of same index to make
         # iterating easier
         matrix = [[w[i] for w in strs] for i in range(0, len(strs[0]))]
+        
         count = 0
         for chars in matrix:
-            curr_str = "".join(chars)
-            sorted_curr_str = "".join(sorted([*curr_str]))
+            sorted_chars = sorted(chars)
 
-            if curr_str != sorted_curr_str:
+            if chars != sorted_chars:
                 count += 1
 
         return count
