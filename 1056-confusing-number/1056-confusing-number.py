7ea5b1b7329aba3db5cae572e630fe5cd7ebf55b
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2023-01-09 00:56:14 -0500

    Time: 30 ms (81.57%), Space: 14 MB (9.72%) - LeetHub

--- 1056-confusing-number/1056-confusing-number.py
+++ 1056-confusing-number/1056-confusing-number.py
@@ -1,19 +1,13 @@
 class Solution:
     def confusingNumber(self, n: int) -> bool:
-        flip = {"0": "0",
-                "1": "1",
-               "6": "9",
-               "8": "8",
-               "9": "6"}
-        
-        str_n = str(n)
-        
-        if any(digit in ("2", "3", "4", "5", "7") for digit in str_n):
-            return False
-        else:
-            new_n = ""
-            
-            for i in range(len(str_n)-1, -1, -1):
-                new_n += flip[str_n[i]]
+        flip = {'0': '0', '1': '1', '6': '9', '8': '8', '9': '6'}
+        digits = [*str(n)]
+        str_n = ""
+        for d in digits:
+            if d in flip.keys():
+                str_n += flip[d]
+            else:
+                return False
 
-            return int(new_n) != n
+        new_n = int(str_n[::-1])
+        return n != new_n
