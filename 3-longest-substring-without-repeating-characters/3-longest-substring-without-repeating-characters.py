8286dd8a11977bc2e1259812e580d4961a6886a9
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2023-01-20 15:44:17 -0500

    Time: 54 ms (94.35%), Space: 14.2 MB (13.55%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,36 +1,18 @@
-from collections import deque
-
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # Covers cases when s is empty, and when there are no dupes in s
-        if len(set(s)) == len(s):
-            return len(s)
-
-        # Sliding window approach
-        # Use a seen list to keep track of said values
-        # deque for dequeueing from the front
-        seen = []
-        substr = deque([])
-        longest = 0  # Keeps track of the longest substring
+        window = deque([])
+        longest = 0
 
         for char in s:
-            if char not in seen:
-                seen.append(char)
-                substr.append(char)
+            if not window or char not in window:
+                window.append(char)
             else:
+                longest = max(len(window), longest)
+                while window and window[0] != char:
+                    window.popleft()
 
-                # If the current char is already found in substr, keep popping off the front until we get that same char, then pop one more time
-                if char in substr:
-                    while substr[0] != char:
-                        substr.popleft()
+                window.popleft()
+                window.append(char)
 
-                    substr.popleft()
-                
-                # Append current character as part of new substr
-                substr.append(char)
-
-            # Update the longest substr count if current substr length is longest
-            longest = max(len(substr), longest)
-
+        longest = max(len(window), longest)
         return longest
-    
