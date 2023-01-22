a68601f003a0d5ed14836b3ba662e29b374aa57c
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2023-01-22 10:10:56 -0500

    Time: 247 ms (58.95%), Space: 16.4 MB (15.13%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
@@ -1,49 +1,18 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-    # To do it in O(n), find the index of the smallest positive number
-    # Then use two pointers, one going to the left and one going to the right
-    
-        res = []
-        start = 0
-        min_val = float('inf')
+        n = len(nums)
+        res = [0] * n
+        left = 0
+        right = n - 1
 
-        for i, val in enumerate(nums):
-            if 0 <= val < min_val:
-                min_val = val
-                start = i
+        for i in range(n-1, -1, -1):
+            if abs(nums[left]) < abs(nums[right]):
+                curr = nums[right]
+                right -= 1
+            else:
+                curr = nums[left]
+                left += 1
 
-        if max(nums) < 0:
-            res = [val ** 2 for val in nums[::-1]]
-        elif start == 0:
-            res = [val ** 2 for val in nums]
-        else:
-            seen = set()
-            left = start - 1
-            right = start
-
-            while len(seen) < len(nums):
-                if abs(nums[left]) <= abs(nums[right]):
-                    if left in seen:
-                        res.append(nums[right] ** 2)
-                        seen.add(right)
-                        if right < len(nums) - 1:
-                            right += 1
-                    else:
-                        res.append(nums[left] ** 2)
-                        seen.add(left)
-
-                    if left > 0:
-                        left -= 1
-                elif abs(nums[right]) < abs(nums[left]):
-                    if right in seen:
-                        res.append(nums[left] ** 2)
-                        seen.add(left)
-                        if left > 0:
-                            left -= 1
-                    else:
-                        res.append(nums[right] ** 2)
-                        seen.add(right)
-                    if right < len(nums) - 1:
-                        right += 1
-
+            res[i] = curr ** 2
+            
         return res
