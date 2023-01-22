5fd2ec69c0fa85842d419287e930e1a16d1dd054
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2023-01-22 11:30:42 -0500

    Time: 57 ms (58.39%), Space: 14.5 MB (63.39%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,7 +1,17 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        convert_s = sorted([*s])
-        convert_t = sorted([*t])
-        
-        return convert_s == convert_t
+        if len(s) != len(t):
+            return False
+
+        occ = [0] * 26
+
+        for i in range(len(s)):
+            occ[ord(s[i]) - 97] += 1
+            occ[ord(t[i]) - 97] -= 1
+
+        for val in occ:
+            if val != 0:
+                return False
+
+        return True
     
