9986c8f1a218a5f83f5f954f489989e80cd81e90
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2023-01-28 22:38:14 -0500

    Time: 133 ms (91.05%), Space: 14.2 MB (64.90%) - LeetHub

--- 914-x-of-a-kind-in-a-deck-of-cards/914-x-of-a-kind-in-a-deck-of-cards.py
+++ 914-x-of-a-kind-in-a-deck-of-cards/914-x-of-a-kind-in-a-deck-of-cards.py
@@ -4,16 +4,18 @@ class Solution:
         for val in deck:
             table[val] = 1 + table.get(val, 0)
 
-        lcf = min(table.values())
+        # Use the lowest common factor as divisor in modulo later
+        cf = min(table.values())
 
         for card in table:
             if table[card] == 1:
                 return False
 
-            gcf = math.gcd(table[card], lcf)
+            # gcf checks if the two numbers can be partitioned into a number that's not 1
+            gcf = math.gcd(table[card], cf)
             if gcf == 1:
                 return False
             else:
-                lcf = min(gcf, lcf)
+                cf = min(gcf, cf)
 
         return True
