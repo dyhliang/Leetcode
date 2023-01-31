6ab9c867c13463b85c62e0082b5f584ba58b6226
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2023-01-31 13:37:20 -0500

    Time: 39 ms, Memory: 13.7 MB - LeetHub

---
+++ n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        
+        table = [0, 1, 1]
+        if n < 3:
+            return table[n]
+        
+        for val in range(3, n+1):
+            table.append(table[-1] + table[-2] + table[-3])
+        
+        return table[-1]
