cbebcdf54cfe0db628db4da30bcb5c822f6f82da
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2023-02-01 22:45:50 -0500

    Time: 342 ms (88.67%), Space: 24.3 MB (44.63%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
@@ -1,5 +1,7 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        # Find the max between the length of nums or +1 from the max
+        # To account for edge case like Example 2 [1,1] where we need the 2.
         table = [0] * max((max(set(nums)) + 1), len(nums) + 1)
         res = []
 
