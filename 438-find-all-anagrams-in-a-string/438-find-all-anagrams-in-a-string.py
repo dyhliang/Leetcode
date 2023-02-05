0057b20a15f99ac7e9cf3131fa31e3004208ce2e
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2023-02-04 19:59:33 -0500

    Time: 9255 ms (5.04%), Space: 15.3 MB (27.00%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -1,19 +1,17 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         window = deque([])
-        indices = deque([])
         res = []
         p_ana = "".join(sorted([*p]))
         
         for i, char in enumerate(s):
             window.append(char)
-            indices.append(i)
+
             if len(window) == len(p):
                 ana = "".join(sorted(window))
                 if ana == p_ana:
-                    res.append(indices[0])
+                    res.append(i - (len(p) - 1))
                     
-                indices.popleft()
                 window.popleft()
 
         return res
