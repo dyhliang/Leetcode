a317a1daadc49e1410d6abd048230c58984b8d53
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2023-02-08 13:08:33 -0500

    Time: 2092 ms (39.67%), Space: 19 MB (11.21%) - LeetHub

---
+++ 15-3sum/15-3sum.py
@@ -0,0 +1,26 @@
+class Solution:
+    def threeSum(self, array: List[int]) -> List[List[int]]:
+        array.sort()
+
+        # Similar to Two sum, we can use a table
+        # look for first number, solve two sum with that first number
+        res = set()
+        seen = set()
+        for i, val in enumerate(array[:-2]):
+            l = i + 1
+            r = len(array) - 1
+            table = {}
+            window = array[l:r + 1]
+            if val not in seen:
+                seen.add(val)
+                for j, v in enumerate(window):
+                    diff = 0 - val - v
+
+                    if v in table.keys():
+                        # [key = value, index = j]
+                        combo = (val, diff, v)
+                        res.add(combo)
+                    else:
+                        table[diff] = j
+
+        return res
