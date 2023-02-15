317c8840f7e40a770be5e96ddf69c12b1ab71691
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2023-02-15 12:06:18 -0500

    Time: 45 ms (63.16%), Space: 15.1 MB (11.53%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.py
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -1,19 +1,21 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        hasht = {3: "Fizz", 5: "Buzz"}
+        table = {
+            3: "Fizz",
+            5: "Buzz"
+        }
+        
         res = []
-        for i in range(1, n+1):
-            string = ""
-            for key in hasht.keys():
-                # Reduces the need for multiple ifs to test 3, 5, and 15
-                if i % key == 0:
-                    string += hasht[key]
+        for val in range(1, n+1):
+            ans = ""
+            for div in table.keys():
+                if val % div == 0:
+                    ans += table[div]
             
-            # If the string wasn't modified, then i is not divisible by 3, 5, and 15
-            if string == "":
-                string = str(i)
-
-            res.append(string)
-                       
+            if ans == "":
+                ans = str(val)
+                
+            res.append(ans)
+            
         return res
     
