1f897df05be4f9731fb4679a65d1ebc0b89f61a8
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2023-02-16 15:03:02 -0500

    Time: 4037 ms (5.00%), Space: 14.1 MB (8.82%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,31 @@
+import copy
+from collections import defaultdict
+
+
+class Solution:
+    def backtrack(self, combo, res, target, candidates, table, seen):
+        if sum(combo) == target:
+            combo_copy = copy.deepcopy(combo)
+            table_copy = copy.deepcopy(table)
+            if table_copy not in seen:
+                res.append(combo_copy)
+                seen.append(table_copy)
+            return
+        elif sum(combo) > target:
+            return
+
+        for val in candidates:
+            combo.append(val)
+            table[val] += 1
+            self.backtrack(combo, res, target, candidates, table, seen)
+            popped = combo.pop()
+            table[popped] -= 1
+            # deletes the entry in the dict if its occ goes to 0
+            if table[popped] == 0:   
+                table.pop(popped)
+
+    def combinationSum(self, candidates: list[int], target: int):
+        res = []
+        self.backtrack([], res, target, candidates, defaultdict(int), [])
+        return res
+    
