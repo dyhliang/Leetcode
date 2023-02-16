471eb1bec8d1941f41c9531562381170d81b68c8
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2023-02-15 22:00:00 -0500

    Time: 50 ms (32.48%), Space: 16.5 MB (18.14%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
@@ -6,7 +6,6 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        
         levels = []
         if not root:
             return 0
@@ -14,14 +13,15 @@ class Solution:
         def traverse(node, i):
             if len(levels) == i:
                 levels.append([])
-            
+                
             levels[i].append(node.val)
-            
             if node.left:
-                traverse(node.left, i + 1)
-            
+                traverse(node.left, i+1)
+                
             if node.right:
-                traverse(node.right, i + 1)
+                traverse(node.right, i+1)
+                
             
         traverse(root, 0)
         return len(levels)
+    
