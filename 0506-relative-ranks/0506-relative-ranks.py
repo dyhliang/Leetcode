f639092a393a1ca523778e9e7800e06aa17d70bd
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2023-02-25 17:21:52 -0500

    Time: 79 ms (59.25%), Space: 15.4 MB (16.75%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findRelativeRanks(self, score: List[int]) -> List[str]:
+        res = []
+        heap = []
+        table = defaultdict()
+        places = {1: "Gold Medal",
+                  2: "Silver Medal",
+                  3: "Bronze Medal"}
+
+        for val in score:
+            heapq.heappush(heap, -val)
+
+        k = 1
+        while heap:
+            popped = heapq.heappop(heap)
+            table[-popped] = k
+            k += 1
+
+        for val in score:
+            if table[val] in places.keys():
+                res.append(places[table[val]])
+            else:
+                res.append(str(table[val]))
+
+        return res
+    
