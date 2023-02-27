b712d7445b9767f04f8b1f94ad3d4b8dbc090df2
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2023-02-27 10:43:14 -0500

    Time: 39 ms (44.67%), Space: 13.8 MB (84.98%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -6,8 +6,5 @@ class Solution:
             if c in table.keys():
                 table[c] += 1
                 
-        if 0 in table.values() or (table['l'] < 2 and table['o'] < 2):
-            return 0
-        else:
-            l_o = min(table['l'] // 2, table['o'] // 2)
-            return min(l_o, min(table.values()))
+        l_o = min(table['l'] // 2, table['o'] // 2)
+        return min(l_o, min(table.values()))
