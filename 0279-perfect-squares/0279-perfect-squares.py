036c0bded4db34c0c42f2673cba6fed9f4774421
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2023-03-07 14:40:35 -0500

    Time: 68 ms (94.92%), Space: 14.4 MB (38.15%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        def is_divided_by(n, count):
+            """
+                return: true if "n" can be decomposed into "count" number of perfect                            square numbers.
+                e.g. n=12, count=3:  true.
+                     n=12, count=2:  false
+            """
+            if count == 1:
+                return n in square_nums
+            
+            for k in square_nums:
+                if is_divided_by(n - k, count - 1):
+                    return True
+            return False
+
+        square_nums = set([i * i for i in range(1, int(n**0.5)+1)])
+    
+        for count in range(1, n+1):
+            if is_divided_by(n, count):
+                return count
+    
