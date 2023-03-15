0cf35b5964a7da6d2d3f6be6458079a7e57fba1e
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2023-03-15 13:18:43 -0400

    Time: 53 ms (65.24%), Space: 14 MB (37.23%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
@@ -1,15 +1,13 @@
-import heapq as hq
-
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
         maxheap = []
         for val in gifts:
-            hq.heappush(maxheap, -val)
+            heapq.heappush(maxheap, -val)
             
         while k > 0:
-            popped = -1 * hq.heappop(maxheap)
+            popped = -1 * heapq.heappop(maxheap)
             rem = -int(popped ** 0.5)
-            hq.heappush(maxheap, rem)
+            heapq.heappush(maxheap, rem)
             k -= 1
             
         return -sum(maxheap)
