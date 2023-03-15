4ef98ce9af11571b85d0f08eeae909e45e7680d9
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2023-03-14 20:16:25 -0400

    Time: 56 ms (70.04%), Space: 13.9 MB (83.61%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -1,13 +1,12 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
         seen = set()
-        fls = [float(v) for v in arr]
         
-        for i, n in enumerate(fls):
-            if n * 2 in seen or n / 2 in seen:
+        for i, n in enumerate(arr):
+            if float(n) * 2 in seen or n / 2 in seen:
                 return True
             
-            seen.add(n)
+            seen.add(float(n))
         
         return False
     
