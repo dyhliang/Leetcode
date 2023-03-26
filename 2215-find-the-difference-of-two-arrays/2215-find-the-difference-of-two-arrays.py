406a000a25cd053bfc38ddb21dee256eae29a104
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2023-03-26 12:56:03 -0400

    Time: 211 ms (38.62%), Space: 14.4 MB (45.01%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
@@ -2,21 +2,21 @@ class Solution:
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
         table1 = defaultdict(int)
         table2 = defaultdict(int)
-        new_nums1 = set()
-        new_nums2 = set()
+        new_nums1 = []
+        new_nums2 = []
         for i, val in enumerate(nums1):          
             table1[val] += 1
             
         for i, val in enumerate(nums2):
             table2[val] += 1
             
-        for i, val in enumerate(nums1):
-            if val not in table2.keys():
-                new_nums1.add(val)
+        for k in table1.keys():
+            if k not in table2.keys():
+                new_nums1.append(k)
                 
-        for i, val in enumerate(nums2):
-            if val not in table1.keys():
-                new_nums2.add(val)
+        for k in table2.keys():
+            if k not in table1.keys():
+                new_nums2.append(k)
         
-        return [list(new_nums1), list(new_nums2)]
+        return [new_nums1, new_nums2]
     
