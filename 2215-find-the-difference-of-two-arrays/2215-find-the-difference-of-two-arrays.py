1918b72ae8ab6445f2f571a46d949284520e64b1
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2023-03-26 12:59:44 -0400

    Time: 183 ms (67.04%), Space: 14.5 MB (9.97%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
@@ -1,22 +1,20 @@
 class Solution:
+    def updateTable(self, arr, table):
+        for n in arr:
+            table[n] == 1
+            
+    def updateNewArr(self, new_arr, ta, tb):
+        for k in ta.keys():
+            if k not in tb.keys():
+                new_arr.append(k)
+    
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
-        table1 = defaultdict(int)
-        table2 = defaultdict(int)
-        new_nums1 = []
-        new_nums2 = []
-        for i, val in enumerate(nums1):          
-            table1[val] += 1
-            
-        for i, val in enumerate(nums2):
-            table2[val] += 1
-            
-        for k in table1.keys():
-            if k not in table2.keys():
-                new_nums1.append(k)
-                
-        for k in table2.keys():
-            if k not in table1.keys():
-                new_nums2.append(k)
+        table1, table2 = defaultdict(int), defaultdict(int)
+        new_nums1, new_nums2 = [], []
+        self.updateTable(nums1, table1)
+        self.updateTable(nums2, table2)
         
+        self.updateNewArr(new_nums1, table1, table2)
+        self.updateNewArr(new_nums2, table2, table1)
         return [new_nums1, new_nums2]
     
