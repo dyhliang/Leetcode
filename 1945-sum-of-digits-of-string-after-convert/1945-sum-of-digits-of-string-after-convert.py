770091f80b0b6924870edc8cdd848575ad21cba9
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2023-03-29 17:06:01 -0400

    Time: 45 ms (13.28%), Space: 13.9 MB (58.40%) - LeetHub

---
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.py
@@ -0,0 +1,17 @@
+class Solution:
+    def getLucky(self, s: str, k: int) -> int:
+        alpha_count = ""
+        for c in s:
+            alpha_count += str(ord(c) - 96)
+            
+        conv_count = alpha_count
+        while k > 0:
+            new_count = 0
+            for d in conv_count:
+                new_count += int(d)
+                
+            conv_count = str(new_count)
+            k -= 1
+            
+        return int(conv_count)
+            
