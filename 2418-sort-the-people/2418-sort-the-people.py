9ef317aa7fb009bdbb34df4e2015808e4a246759
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2023-04-07 10:15:32 -0400

    Time: 126 ms (28.65%), Space: 14.4 MB (35.71%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -2,7 +2,6 @@ import heapq as hq
 
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        
         maxheap = []
         for i, name in enumerate(names):
             hq.heappush(maxheap, [-heights[i], name])
