d5163e2a215d1a2c8f34a7e5e879bb0b39c0d7c0
Dan Yu Liang <102781030+dyhliang@users.noreply.github.com>
2023-05-27 22:18:28 -0400

    Time: 145 ms (46.77%), Space: 17.7 MB (19.03%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,17 +1,14 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         max_num = max(nums)
-        occ = defaultdict(int)
+        occ = [0] * (max_num + 1)
         ans = max_num + 1
-        
-        for n in range(max_num + 1):
-            occ[n] = 0
-            
+
         for j in nums:
             occ[j] += 1
             
-        for k in occ:
-            if occ[k] == 0:
-                return k
+        for i, n in enumerate(occ):
+            if n == 0:
+                return i
             
         return ans
